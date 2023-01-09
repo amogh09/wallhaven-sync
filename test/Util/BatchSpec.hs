8f29bb63f0a78016dce80d8a3508200f5d316123
Amogh Rathore <amoghdroid09@gmail.com>
2023-01-08 21:06:13 -0800

    Add some comments

--- test/Util/BatchSpec.hs
+++ test/Util/BatchSpec.hs
@@ -10,12 +10,17 @@ spec = do
   describe "batchedM" $ do
     it "performs the action in batches" $ do
       let size = 5
+      -- simulates a list of limited resources
       resources <- newMVar $ replicate size 0
+      -- this action uses a resource from resources and returns it
+      -- back after a delay.
       let action (x :: Int) = do
             modifyMVar_ resources (evaluate . tail)
             threadDelay 1000
             modifyMVar_ resources (evaluate . (x :))
             pure x
+      -- test that action can be performed safely in batches of
+      -- size equal to the number of available resources.
       res <- batchedM size action [1 .. 100]
       res `shouldBe` [1 .. 100]
       resources' <- readMVar resources
