c5236dff0145ecfbccb9db8dcfc37edd16b6121f
Amogh Rathore <amoghdroid09@gmail.com>
2023-01-22 09:26:40 -0800

    Move interface implementations to new modules and clean-up Env as a result

--- app/Wallhaven/Env.hs
+++ app/Wallhaven/Env.hs
@@ -1,41 +1,31 @@
 module Wallhaven.Env (Env (..), Config (..)) where
 
 import Control.Monad.Reader (ReaderT, asks)
-import qualified Network.HTTP.Conduit as HTTP
-import qualified Network.HTTP.Simple as HTTP
-import Network.HTTP.Types (unauthorized401)
-import Network.HTTP.Types.Status (notFound404)
-import System.FilePath ((</>))
-import System.IO.Error (isPermissionError)
-import Types (FullWallpaperURL, Label, Username, WallpaperName)
 import qualified Types
-import UnliftIO (MonadUnliftIO, catch, throwIO)
-import UnliftIO.Directory (createDirectoryIfMissing, listDirectory)
-import UnliftIO.IO.File (writeBinaryFile)
-import Util.FileSystem (deleteFileIfExists)
-import qualified Wallhaven.API.Action as WallhavenAPI
-import Wallhaven.API.Exception (CollectionURLsFetchException (..))
-import Wallhaven.Exception (WallhavenSyncException (..))
+import UnliftIO (MonadUnliftIO)
+import qualified Wallhaven.Implementation.API as API
+import qualified Wallhaven.Implementation.FileSystemDB as FileSystemDB
 import Wallhaven.Monad
 import Prelude hiding (log)
 
+-- Environment record
 data Env = Env
   { envConfig :: !Config,
     envLog :: !(String -> IO ())
   }
 
--- Configuration structure.
+-- Configuration record
 data Config = Config
   { -- | The directory where the wallpapers will be saved.
-    configWallpaperDir :: FilePath,
+    configWallpaperDir :: !FilePath,
     -- | The number of wallpapers to download in parallel.
-    configNumParallelDownloads :: Types.NumParallelDownloads,
+    configNumParallelDownloads :: !Types.NumParallelDownloads,
     -- | Whether to delete unliked wallpapers.
-    configDeleteUnliked :: Bool,
+    configDeleteUnliked :: !Bool,
     -- | Wallhaven API Key
-    configWallhavenAPIKey :: String,
+    configWallhavenAPIKey :: !String,
     -- | Debug mode
-    configDebug :: Bool
+    configDebug :: !Bool
   }
 
 instance HasDebug Env where
@@ -53,173 +43,24 @@ instance HasSyncParallelization Env where
 instance (MonadUnliftIO m) => MonadGetCollectionURLs (ReaderT Env m) where
   getCollectionURLs username label = do
     apiKey <- asks (configWallhavenAPIKey . envConfig)
-    catch
-      (WallhavenAPI.getAllCollectionURLs apiKey username label)
-      (throwIO . collectionFetchExceptionHandler username label)
-
-collectionFetchExceptionHandler ::
-  Username -> Label -> CollectionURLsFetchException -> WallhavenSyncException
-collectionFetchExceptionHandler
-  username
-  label
-  ( UserCollectionsHTTPException
-      (HTTP.HttpExceptionRequest _ (HTTP.StatusCodeException res _))
-    )
-    | HTTP.getResponseStatus res == unauthorized401 =
-        let oneLine =
-              "not authorized to access user collections,"
-                <> " is your API key valid?"
-            verbose = show res
-         in CollectionFetchException username label oneLine verbose
-    | HTTP.getResponseStatus res == notFound404 =
-        let oneLine =
-              "no collections found for user "
-                <> username
-                <> ","
-                <> " is the username valid?"
-            verbose = show res
-         in CollectionFetchException username label oneLine verbose
-collectionFetchExceptionHandler
-  username
-  label
-  (UserCollectionsHTTPException e) =
-    let oneLine = "HTTP request to list user collections failed"
-     in CollectionFetchException username label oneLine (show e)
-collectionFetchExceptionHandler
-  username
-  label
-  (UserCollectionsParseException jsonErr) =
-    let oneLine = "failed to parse user collections API response"
-     in CollectionFetchException username label oneLine jsonErr
-collectionFetchExceptionHandler
-  username
-  label
-  (CollectionNotFoundException _) =
-    let oneLine = "collection " <> label <> " was not found"
-     in CollectionFetchException username label oneLine oneLine
-collectionFetchExceptionHandler
-  username
-  label
-  (CollectionFetchHTTPException cid page e) =
-    let oneLine =
-          "HTTP request to fetch page "
-            <> show page
-            <> " of collection with ID "
-            <> show cid
-            <> " failed"
-     in CollectionFetchException username label oneLine (show e)
-collectionFetchExceptionHandler
-  username
-  label
-  (MetaParseException cid jsonErr) =
-    let oneLine =
-          "failed to parse collection metadata from API response"
-            <> " for collection with ID "
-            <> show cid
-     in CollectionFetchException username label oneLine jsonErr
-collectionFetchExceptionHandler
-  username
-  label
-  (WallpaperURLsParseException jsonErr) =
-    let oneLine = "failed to parse wallpaper URLs from API response"
-     in CollectionFetchException username label oneLine jsonErr
+    API.getCollectionURLs apiKey username label
 
 instance MonadUnliftIO m => MonadDownloadWallpaper (ReaderT Env m) where
-  downloadWallpaper url =
-    catch
-      (WallhavenAPI.getFullWallpaper url)
-      (throwIO . wallpaperDownloadExceptionHandler url)
-
-wallpaperDownloadExceptionHandler ::
-  FullWallpaperURL -> HTTP.HttpException -> WallhavenSyncException
-wallpaperDownloadExceptionHandler url e =
-  let oneLine = "HTTP request to download wallpaper failed"
-   in WallpaperDownloadException url oneLine (show e)
+  downloadWallpaper = API.downloadWallpaper
 
 instance (MonadUnliftIO m) => MonadDeleteWallpaper (ReaderT Env m) where
-  deleteWallpaper name = do
-    dir <- asks (configWallpaperDir . envConfig)
-    catch
-      (deleteFileIfExists $ dir </> name)
-      (throwIO . deleteWallpaperExceptionHandler dir name)
-
-deleteWallpaperExceptionHandler ::
-  FilePath -> WallpaperName -> IOError -> WallhavenSyncException
-deleteWallpaperExceptionHandler dir name e
-  | isPermissionError e =
-      let oneLine =
-            "no permission to delete wallpaper file " <> (dir </> name)
-          verbose = show e
-       in DeleteWallpaperException name oneLine verbose
-  | otherwise =
-      DeleteWallpaperException
-        name
-        ("failed to delete wallpaper file " <> (dir </> name))
-        (show e)
+  deleteWallpaper name =
+    asks (configWallpaperDir . envConfig)
+      >>= flip FileSystemDB.deleteWallpaper name
 
 instance (MonadUnliftIO m) => MonadSaveWallpaper (ReaderT Env m) where
   saveWallpaper name wallpaper = do
     dir <- asks (configWallpaperDir . envConfig)
-    catch
-      (writeBinaryFile (dir </> name) wallpaper)
-      (throwIO . saveWallpaperExceptionHandler dir name)
-
-saveWallpaperExceptionHandler ::
-  FilePath -> WallpaperName -> IOError -> WallhavenSyncException
-saveWallpaperExceptionHandler dir name e
-  | isPermissionError e =
-      let oneLine =
-            "no permission to save wallpaper to file " <> (dir </> name)
-          verbose = show e
-       in SaveWallpaperException name oneLine verbose
-  | otherwise =
-      SaveWallpaperException
-        name
-        ("failed to save wallpaper to file " <> (dir </> name))
-        (show e)
+    FileSystemDB.saveWallpaper dir name wallpaper
 
 instance (MonadUnliftIO m) => MonadInitDB (ReaderT Env m) where
-  initDB = do
-    dir <- asks (configWallpaperDir . envConfig)
-    catch
-      (createDirectoryIfMissing True dir)
-      (throwIO . initDBExceptionHandler dir)
+  initDB = asks (configWallpaperDir . envConfig) >>= FileSystemDB.initDB
 
-initDBExceptionHandler :: FilePath -> IOError -> WallhavenSyncException
-initDBExceptionHandler dir e
-  | isPermissionError e =
-      let oneLine =
-            "no permission to create wallpaper directory '"
-              <> dir
-              <> "'"
-          verbose = show e
-       in InitDBException oneLine verbose
-  | otherwise =
-      InitDBException
-        ("failed to create wallpaper directory '" <> dir <> "'")
-        (show e)
-
-instance
-  (MonadUnliftIO m) =>
-  MonadGetDownloadedWallpapers (ReaderT Env m)
-  where
-  getDownloadedWallpapers = do
-    dir <- asks (configWallpaperDir . envConfig)
-    catch
-      (listDirectory dir)
-      (throwIO . getDownloadedWallpapersExceptionHandler dir)
-
-getDownloadedWallpapersExceptionHandler ::
-  FilePath -> IOError -> WallhavenSyncException
-getDownloadedWallpapersExceptionHandler dir e
-  | isPermissionError e =
-      let oneLine =
-            "no permission to list contents of wallpaper directory '"
-              <> dir
-              <> "'"
-          verbose = show e
-       in InitDBException oneLine verbose
-  | otherwise =
-      InitDBException
-        ("failed to list contents of wallpaper directory '" <> dir <> "'")
-        (show e)
+instance (MonadUnliftIO m) => MonadGetDownloadedWallpapers (ReaderT Env m) where
+  getDownloadedWallpapers =
+    asks (configWallpaperDir . envConfig) >>= FileSystemDB.getDownloadedWallpapers
---
+++ src/Wallhaven/Implementation/API.hs
@@ -0,0 +1,102 @@
+-- Provides functions that implement Wallhaven interaction interface to be used when
+-- constructing environments.
+module Wallhaven.Implementation.API (getCollectionURLs, downloadWallpaper) where
+
+import Data.ByteString (ByteString)
+import qualified Network.HTTP.Client.Conduit as HTTP
+import qualified Network.HTTP.Simple as HTTP
+import Network.HTTP.Types.Status (notFound404, unauthorized401)
+import Types (FullWallpaperURL, Label, Username)
+import UnliftIO (MonadUnliftIO, catch, throwIO)
+import qualified Wallhaven.API.Action as WallhavenAPI
+import Wallhaven.API.Exception (CollectionURLsFetchException (..))
+import Wallhaven.Exception
+  ( WallhavenSyncException (CollectionFetchException, WallpaperDownloadException),
+  )
+
+getCollectionURLs ::
+  (MonadUnliftIO m) => String -> Username -> Label -> m [FullWallpaperURL]
+getCollectionURLs apiKey username label =
+  catch
+    (WallhavenAPI.getAllCollectionURLs apiKey username label)
+    (throwIO . collectionFetchExceptionHandler username label)
+
+collectionFetchExceptionHandler ::
+  Username -> Label -> CollectionURLsFetchException -> WallhavenSyncException
+collectionFetchExceptionHandler
+  username
+  label
+  ( UserCollectionsHTTPException
+      (HTTP.HttpExceptionRequest _ (HTTP.StatusCodeException res _))
+    )
+    | HTTP.getResponseStatus res == unauthorized401 =
+        let oneLine =
+              "not authorized to access user collections,"
+                <> " is your API key valid?"
+            verbose = show res
+         in CollectionFetchException username label oneLine verbose
+    | HTTP.getResponseStatus res == notFound404 =
+        let oneLine =
+              "no collections found for user "
+                <> username
+                <> ","
+                <> " is the username valid?"
+            verbose = show res
+         in CollectionFetchException username label oneLine verbose
+collectionFetchExceptionHandler
+  username
+  label
+  (UserCollectionsHTTPException e) =
+    let oneLine = "HTTP request to list user collections failed"
+     in CollectionFetchException username label oneLine (show e)
+collectionFetchExceptionHandler
+  username
+  label
+  (UserCollectionsParseException jsonErr) =
+    let oneLine = "failed to parse user collections API response"
+     in CollectionFetchException username label oneLine jsonErr
+collectionFetchExceptionHandler
+  username
+  label
+  (CollectionNotFoundException _) =
+    let oneLine = "collection " <> label <> " was not found"
+     in CollectionFetchException username label oneLine oneLine
+collectionFetchExceptionHandler
+  username
+  label
+  (CollectionFetchHTTPException cid page e) =
+    let oneLine =
+          "HTTP request to fetch page "
+            <> show page
+            <> " of collection with ID "
+            <> show cid
+            <> " failed"
+     in CollectionFetchException username label oneLine (show e)
+collectionFetchExceptionHandler
+  username
+  label
+  (MetaParseException cid jsonErr) =
+    let oneLine =
+          "failed to parse collection metadata from API response"
+            <> " for collection with ID "
+            <> show cid
+     in CollectionFetchException username label oneLine jsonErr
+collectionFetchExceptionHandler
+  username
+  label
+  (WallpaperURLsParseException jsonErr) =
+    let oneLine = "failed to parse wallpaper URLs from API response"
+     in CollectionFetchException username label oneLine jsonErr
+
+downloadWallpaper ::
+  (MonadUnliftIO m) => FullWallpaperURL -> m ByteString
+downloadWallpaper url =
+  catch
+    (WallhavenAPI.getFullWallpaper url)
+    (throwIO . wallpaperDownloadExceptionHandler url)
+
+wallpaperDownloadExceptionHandler ::
+  FullWallpaperURL -> HTTP.HttpException -> WallhavenSyncException
+wallpaperDownloadExceptionHandler url e =
+  let oneLine = "HTTP request to download wallpaper failed"
+   in WallpaperDownloadException url oneLine (show e)
